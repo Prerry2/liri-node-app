646969c666c1a2df215294aadbfa8dba8b8eff36
Prerry2 <anonybe@gmail.com>
2018-10-09 19:50:11 -0400

    Added movie and concert functionalities.

--- liri.js
+++ liri.js
@@ -11,8 +11,8 @@ var typeOfRequest = process.argv[2];
 var requestSpecific = process.argv[3];
 
 console.log("If there is an error the first time running the program, it'll likely be fixed by trying again...")
-
 if (typeOfRequest === "do-what-it-says") {
+  console.log("Just DO IT! Don't let your dreams be dreams!")
   var whatItSaysArray = [];
   fs.readFile("random.txt", "ascii", function(err, data) {
     if (err) {
@@ -21,38 +21,60 @@ if (typeOfRequest === "do-what-it-says") {
     whatItSaysArray = data.split(",");
   });
   setTimeout(function() {
-  // console.log(whatItSaysArray);
-  typeOfRequest = whatItSaysArray[0];
-  requestSpecific = whatItSaysArray[1];
-  // console.log(typeOfRequest + " " + requestSpecific);
-}, 50); }
-// Inserted delay to fix issues with .then not working and variables above not being set quickly enough...
+    // console.log(whatItSaysArray);
+    typeOfRequest = whatItSaysArray[0];
+    requestSpecific = whatItSaysArray[1];
+    // console.log(typeOfRequest + " " + requestSpecific);
+  }, 50); }
+  // Inserted delay to fix issues with .then not working and variables above not being set quickly enough...
+  // No, I have no idea why the two delays are BOTH necessary for this whole thing to work...
 setTimeout(function() {
 if (typeOfRequest === "movie-this") {
   console.log("Movie selected")
+  if (!requestSpecific) {
+    requestSpecific = 'Mr. Nobody'
+  }
   var queryUrl =
     "http://www.omdbapi.com/?t=" +
     requestSpecific +
     "&y=&plot=short&apikey=trilogy";
-  request(queryUrl, function(error, response, body) {
+  request(queryUrl, function(error, _, body) {
     if (error) {
       console.log(error);
     }
-    // Do stuff here
+    var jsonData = JSON.parse(body)
+    var movieData = [
+      "Title: " + jsonData[0].Title,
+      "Year: " + jsonData[0].Year,
+      "IMDB Rating: " + jsonData[0].imdbRating,
+      "Rotten Tomatoes Rating: " + jsonData[0].Ratings[1].Value,
+      "Country of Production: " + jsonData[0].Country,
+      "Language: " + jsonData[0].Language,
+      "Plot: " + jsonData[0].Plot,
+      "Actors: " + jsonData[0].Actors
+    ].join('\n\n')
+    console.log(movieData)
   });
 } else if (typeOfRequest === "spotify-this-song") {
   console.log("Spotify selected")
+  
 } else if (typeOfRequest === "concert-this") {
   console.log("Concert selected")
   var queryUrl =
     "https://rest.bandsintown.com/artists/" +
     requestSpecific +
     "/events?app_id=codingbootcamp";
-  request(queryUrl, function(error, response, body) {
+  request(queryUrl, function(error, _, body) {
     if (error) {
       console.log(error);
     }
-    // Do stuff here
+    var jsonData = JSON.parse(body)
+    var concertData = [
+      "Venue name: " + jsonData[0].venue.name,
+      "Location: " + jsonData[0].venue.city + ", " + jsonData[0].venue.region,
+      "Date: " + moment(jsonData[0].datetime).format("MM/DD/YYYY")
+    ].join("\n\n")
+    console.log(concertData)
   });
 } else {
   console.log(
