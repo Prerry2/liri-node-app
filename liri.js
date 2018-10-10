ec2e97819e8ac77c6120865c7be44cb6da0fb8b4
Prerry2 <anonybe@gmail.com>
2018-10-09 20:06:14 -0400

    Split up the uber code block.

--- liri.js
+++ liri.js
@@ -10,9 +10,11 @@ var spotify = new Spotify(keys.spotify);
 var typeOfRequest = process.argv[2];
 var requestSpecific = process.argv[3];
 
-console.log("If there is an error the first time running the program, it'll likely be fixed by trying again...")
+console.log(
+  "If there is an error the first time running the program, it'll likely be fixed by trying again... #computers"
+);
 if (typeOfRequest === "do-what-it-says") {
-  console.log("Just DO IT! Don't let your dreams be dreams!")
+  console.log("Just DO IT! Don't let your dreams be dreams!");
   var whatItSaysArray = [];
   fs.readFile("random.txt", "ascii", function(err, data) {
     if (err) {
@@ -25,14 +27,17 @@ if (typeOfRequest === "do-what-it-says") {
     typeOfRequest = whatItSaysArray[0];
     requestSpecific = whatItSaysArray[1];
     // console.log(typeOfRequest + " " + requestSpecific);
-  }, 50); }
+  }, 50);
+  setTimeout(ifBlock(), 60);
   // Inserted delay to fix issues with .then not working and variables above not being set quickly enough...
   // No, I have no idea why the two delays are BOTH necessary for this whole thing to work...
-setTimeout(function() {
-if (typeOfRequest === "movie-this") {
-  console.log("Movie selected")
+} else {
+  ifBlock()
+}
+
+var movieFn = function() {
   if (!requestSpecific) {
-    requestSpecific = 'Mr. Nobody'
+    requestSpecific = "Mr. Nobody";
   }
   var queryUrl =
     "http://www.omdbapi.com/?t=" +
@@ -42,7 +47,7 @@ if (typeOfRequest === "movie-this") {
     if (error) {
       console.log(error);
     }
-    var jsonData = JSON.parse(body)
+    var jsonData = JSON.parse(body);
     var movieData = [
       "Title: " + jsonData[0].Title,
       "Year: " + jsonData[0].Year,
@@ -52,15 +57,10 @@ if (typeOfRequest === "movie-this") {
       "Language: " + jsonData[0].Language,
       "Plot: " + jsonData[0].Plot,
       "Actors: " + jsonData[0].Actors
-    ].join('\n\n')
-    console.log(movieData)
-  });
-} else if (typeOfRequest === "spotify-this-song") {
-  console.log("Spotify selected")
-  
-} else if (typeOfRequest === "concert-this") {
-  console.log("Concert selected")
-  var queryUrl =
+    ].join("\n\n");
+    console.log(movieData);
+  });}
+var concertFn = function () {var queryUrl =
     "https://rest.bandsintown.com/artists/" +
     requestSpecific +
     "/events?app_id=codingbootcamp";
@@ -68,19 +68,35 @@ if (typeOfRequest === "movie-this") {
     if (error) {
       console.log(error);
     }
-    var jsonData = JSON.parse(body)
+    var jsonData = JSON.parse(body);
     var concertData = [
       "Venue name: " + jsonData[0].venue.name,
       "Location: " + jsonData[0].venue.city + ", " + jsonData[0].venue.region,
       "Date: " + moment(jsonData[0].datetime).format("MM/DD/YYYY")
-    ].join("\n\n")
-    console.log(concertData)
+    ].join("\n\n");
+    console.log(concertData);
   });
-} else {
+} 
+var spotifyFn = function () {
+  
+}
+
+
+var ifBlock = function() {
+  if (typeOfRequest === "movie-this") {
+    console.log("Movie selected");
+    movieFn()
+  } else if (typeOfRequest === "spotify-this-song") {
+    console.log("Spotify selected");
+    spotifyFn()
+  } else if (typeOfRequest === "concert-this") {
+    console.log("Concert selected");
+    concertFn()
+  } else {
   console.log(
     "Syntax error: typeOfRequest " +
       typeOfRequest +
       " is NOT an accepted argument. \nTry 'concert-this', 'movie-this', 'spotify-this', or 'do-what-it-says'"
   );
+  }
 }
-}, 60)
